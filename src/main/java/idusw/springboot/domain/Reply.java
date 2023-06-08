795ce8fd0941c6491e266500a1531dd21f13e13f
shcho0123 <jsh980123@gmail.com>
2023-06-08 16:55:45 +0900

    230608

---
+++ src/main/java/idusw/springboot/domain/Reply.java
@@ -0,0 +1,25 @@
+package idusw.springboot.domain;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@ToString
+@EqualsAndHashCode
+
+public class Reply {
+    private Long rno;
+    private String text;
+    private String replier;
+
+    private Long bno;
+
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
+}
---
+++ src/main/java/idusw/springboot/service/BoardServiceImpl.java
@@ -0,0 +1,71 @@
+package idusw.springboot.service;
+
+
+import idusw.springboot.domain.Board;
+import idusw.springboot.domain.PageRequestDTO;
+import idusw.springboot.domain.PageResultDTO;
+import idusw.springboot.entity.BoardEntity;
+import idusw.springboot.entity.MemberEntity;
+import idusw.springboot.repository.BoardRepository;
+import idusw.springboot.repository.ReplyRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+@Service
+public class BoardServiceImpl implements BoardService {
+    private final BoardRepository boardRepository;
+    private final ReplyRepository replyRepository;
+
+    //public BoardServiceImpl(BoardRepository boardRepository) {this.boardRepository = boardRepository;}
+
+    @Override
+    public int registerBoard(Board board) {
+
+        BoardEntity entity = dtoToEntity(board);
+
+        if(boardRepository.save(entity) != null) // 저장 성공
+            return 1;
+        else
+            return 0;
+    }
+
+    @Override
+    public Board findBoardById(Board board) {
+        Object[] entities = (Object[]) boardRepository.getBoardByBno(board.getBno());
+        return entityToDto((BoardEntity) entities[0], (MemberEntity) entities[1], (Long) entities[2]);
+    }
+
+    @Override
+    public PageResultDTO<Board, Object[]> findBoardAll(PageRequestDTO pageRequestDTO) {
+        Pageable pageable = pageRequestDTO.getPageable(Sort.by("bno").descending());
+        Page<Object[]> result = boardRepository.searchPage(
+                pageRequestDTO.getType(),
+                pageRequestDTO.getKeyword(),
+                pageRequestDTO.getPageable(Sort.by("bno").descending()));
+        Function<Object[], Board> fn = (entity -> entityToDto((BoardEntity) entity[0],
+                (MemberEntity) entity[1], (Long) entity[2]));
+        return new PageResultDTO<>(result, fn, 5);
+    }
+
+    @Transactional
+    @Override
+    public int updateBoard(Board board) {
+        return 0;
+    }
+
+    @Transactional
+    @Override
+    public int deleteBoard(Board board) {
+        replyRepository.deleteByBno(board.getBno()); // 댓글 먼저 삭제
+        boardRepository.deleteById(board.getBno()); // 게시물 삭제
+        return 0;
+    }
+}
